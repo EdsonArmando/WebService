9c62190e1f350664b5e3fa10ee96127ac010d602
Eguix-2010302 <Eguix-2010302@kinal.edu.gt>
2016-05-24 19:26:09 -0600

    Agregando web token

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * Created by Armando on 06/05/2016.
  */
 module.exports={
-    'secret':"a6sf1as1a6f1f6a51a",
+    'secret':"12e3413ddb60b4124e405df724cad65d",
     'database':{
         connectionLimit : 10,
         host : 'localhost',
--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,8 +1,33 @@
 /**
  * Created by Armando on 24/05/2016.
  */
+var jwt=require('jsonwebtoken');
 module.exports = function (app) {
     return{
+        tokenGenerator:function(req,res){
+            var token=jwt.sign({company:'Kinal'},'S3CUR3@APP');
+            res.send(token);
+        },
+        tokenMiddleware:function(req,res,next){
+            var token=req.headers['x-access-token'] || req.body.token || req.query.token;
+            if(token){
+                jwt.verify(token,'S3CUR3@APP',function(err,decoded){
+                    if(err){
+                        return res.status(403).send({
+                            success:false,
+                            message:'Fallo al validar token'
+                        });
+                    }
+                    req.user=decoded;
+                    next();
+                });
+            }else{
+                return res.status(403).send({
+                    success:false,
+                    message:'No se proporciono token'
+                });
+            }
+        },
         add: function(req, res){
             var Usuario = app.get('usuario');
             Usuario.create({
@@ -21,4 +46,21 @@ module.exports = function (app) {
             });
         }
     }
+}
+
+function expiresIn(dias){
+    var dateObj=new Date();
+    return dateObj.setDate(dateObj.getDate()+dias);
+}
+function genToken(user){
+    var payload=jwt.sign({
+            "company":"Kinal"
+        },
+        'S3CUR3@APP');
+    var token={
+        "token":payload,
+        "user":user,
+        "exp": expiresIn(1)
+    }
+    return token;
 }
